2a3be1443e1c1145da138ac7bc2df7816bc889dd
Andy Edwards <jedwards@fastmail.com>
2024-04-09 10:51:03 -0500

    fix: improve stack traces

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,11 +3,28 @@ export type UntilCondition<T> = (
   result?: T
 ) => boolean | Promise<boolean>
 
-export type Poller<T> = Promise<T> & {
+export class Poller<T> {
+  then<TResult1 = T, TResult2 = never>(
+    onfulfilled?:
+      | ((value: T) => TResult1 | PromiseLike<TResult1>)
+      | undefined
+      | null,
+    onrejected?:
+      | ((reason: any) => TResult2 | PromiseLike<TResult2>)
+      | undefined
+      | null
+  ): Promise<TResult1 | TResult2>
+  catch<TResult = never>(
+    onrejected?:
+      | ((reason: any) => TResult | PromiseLike<TResult>)
+      | undefined
+      | null
+  ): Promise<T | TResult>
+  finally(onfinally?: (() => void) | undefined | null): Promise<T>
   cancel(): void
-  until(condition: UntilCondition<T>): Poller<T>
-  timeout(ms: number): Poller<T>
-  noWrapError(): Poller<T>
+  until(condition: UntilCondition<T>): this
+  timeout(ms: number): this
+  noWrapError(): this
 }
 
 export type CallContext<T> = {
--- src/index.js
+++ src/index.js
@@ -5,11 +5,136 @@ export type UntilCondition<T> = (
   result?: T
 ) => boolean | Promise<boolean>
 
-export type Poller<T> = Promise<T> & {
-  cancel(): void,
-  until(condition: UntilCondition<T>): Poller<T>,
-  timeout(ms: number): Poller<T>,
-  noWrapError(): Poller<T>,
+export class Poller<T> {
+  _fn: (info: CallContext<T>) => T | Promise<T>
+  _until: (UntilCondition<T>) => boolean = (error: ?Error, result?: T) => !error
+  _interval: number
+  _timeout: ?number
+  _timeoutId: ?any
+  _wrapError: boolean = true
+  _canceled: boolean = false
+  _fail: (error: Error) => void
+
+  constructor(fn: (info: CallContext<T>) => T | Promise<T>, interval: number) {
+    if (!Number.isFinite(interval) || interval < 0) {
+      throw new Error(`invalid interval: ${interval}`)
+    }
+
+    this._fn = fn
+    this._interval = interval
+  }
+
+  async _run(): Promise<T> {
+    let attemptNumber = 0
+    const startTime = Date.now()
+    let timeoutId: any
+    let passed: ?[T], failed: ?[Error]
+    let fail: (error: Error) => void, pass: (value: T) => void
+    const manualPromise = new Promise<T>(
+      (resolve: (value: T) => void, reject: (error: Error) => void) => {
+        pass = (value: T) => {
+          passed = [value]
+          resolve(value)
+        }
+        this._fail = fail = (error: Error) => {
+          failed = [error]
+          reject(error)
+        }
+        if (this._canceled) fail(new Error('polling canceled'))
+      }
+    )
+    try {
+      // eslint-disable-next-line no-constant-condition
+      while (true) {
+        let result: ?T = undefined,
+          error: ?Error = undefined
+        const now = Date.now()
+        try {
+          result = await Promise.race([
+            this._fn({
+              attemptNumber: attemptNumber++,
+              elapsedTime: now - startTime,
+              fail,
+              pass,
+            }),
+            manualPromise,
+          ])
+        } catch (err) {
+          error = err
+        }
+        if (passed) return passed[0]
+        if (failed) throw failed[0]
+
+        if (await this._until(error, result)) {
+          if (error) throw error
+          return result
+        }
+        const nextTime = now + this._interval
+        if (this._timeout != null && nextTime - startTime > this._timeout) {
+          let message = 'timed out waiting for polling to succeed'
+          if (this._wrapError) {
+            if (error) message += `; last error: ${error.stack}`
+            throw new Error(message)
+          } else {
+            throw error || new Error(message)
+          }
+        } else {
+          const delay = Math.max(0, nextTime - Date.now())
+          await new Promise<void>((resolve: () => void) => {
+            timeoutId = setTimeout(resolve, delay)
+          })
+        }
+      }
+    } finally {
+      if (timeoutId != null) clearTimeout(timeoutId)
+    }
+  }
+
+  then<TResult1 = T, TResult2 = never>(
+    onfulfilled?:
+      | ((value: T) => TResult1 | PromiseLike<TResult1>)
+      | undefined
+      | null,
+    onrejected?:
+      | ((reason: any) => TResult2 | PromiseLike<TResult2>)
+      | undefined
+      | null
+  ): Promise<TResult1 | TResult2> {
+    return this._run().then(onfulfilled, onrejected)
+  }
+
+  catch<TResult = never>(
+    onrejected?:
+      | ((reason: any) => TResult | PromiseLike<TResult>)
+      | undefined
+      | null
+  ): Promise<T | TResult> {
+    return this._run().catch(onrejected)
+  }
+
+  finally(onfinally?: (() => void) | undefined | null): Promise<T> {
+    return this._run().finally(onfinally)
+  }
+
+  cancel() {
+    this._canceled = true
+    this._fail?.(new Error('polling canceled'))
+  }
+
+  until(condition: UntilCondition<T>): this {
+    this._until = condition
+    return this
+  }
+
+  timeout(ms: number): this {
+    this._timeout = ms
+    return this
+  }
+
+  noWrapError(): this {
+    this._wrapError = false
+    return this
+  }
 }
 
 export type CallContext<T> = {
@@ -22,87 +147,7 @@ function poll<T>(
   fn: (info: CallContext<T>) => T | Promise<T>,
   interval: number
 ): Poller<T> {
-  let fail, pass
-  let attemptNumber = 0
-  let until = (error: ?Error, result?: T) => !error
-  let timeout: ?number
-  let timeoutId: ?any
-  let lastError: ?Error
-  let wrapError: boolean = true
-
-  if (!Number.isFinite(interval) || interval < 0) {
-    throw new Error(`invalid interval: ${interval}`)
-  }
-
-  const promise = new Promise(
-    (resolve: (value: T) => void, reject: (error: Error) => void) => {
-      const startTime = Date.now()
-
-      fail = (error: Error) => {
-        if (timeoutId != null) clearTimeout(timeoutId)
-        reject(error)
-      }
-      pass = (value: T) => {
-        if (timeoutId != null) clearTimeout(timeoutId)
-        resolve(value)
-      }
-
-      async function _attempt(): Promise<void> {
-        let result, error
-        const now = Date.now()
-        try {
-          result = await fn({
-            attemptNumber: attemptNumber++,
-            elapsedTime: now - startTime,
-            fail,
-            pass,
-          })
-        } catch (err) {
-          lastError = error = err
-        }
-
-        if (await until(error, result)) {
-          if (timeoutId != null) clearTimeout(timeoutId)
-          if (error) reject(error)
-          else resolve((result: any))
-        } else {
-          const nextTime = now + interval
-          if (timeout != null && nextTime - startTime > timeout) {
-            let message = 'timed out waiting for polling to succeed'
-            if (wrapError) {
-              if (lastError) message += `; last error: ${lastError.stack}`
-              reject(new Error(message))
-            } else {
-              reject(lastError || new Error(message))
-            }
-          } else {
-            const delay = Math.max(0, nextTime - Date.now())
-            timeoutId = setTimeout(attempt, delay)
-          }
-        }
-      }
-
-      const attempt = () => _attempt().catch(() => {})
-
-      attempt()
-    }
-  )
-
-  ;(promise: any).cancel = () => fail(new Error('polling canceled'))
-  ;(promise: any).until = (condition: UntilCondition<T>): Promise<T> => {
-    until = condition
-    return promise
-  }
-  ;(promise: any).timeout = (ms: number): Promise<T> => {
-    timeout = ms
-    return promise
-  }
-  ;(promise: any).noWrapError = (): Promise<T> => {
-    wrapError = false
-    return promise
-  }
-
-  return (promise: any)
+  return new Poller(fn, interval)
 }
 poll.default = poll
 
